26786dc987f412bea58465a076c11da291655970
jimypavel <jimy_jimy_16@yahoo.com>
2013-03-21 20:02:01 +0200

    Interfata dintre Network si Mediator definita

--- src/interfaces/INetworkMediator.java
+++ src/interfaces/INetworkMediator.java
@@ -1,5 +1,19 @@
 package interfaces;
+import classes.*;
+
 // this interfaces the communication between Network module and Mediator module
 public interface INetworkMediator {
 
+	// method for checking if exists an user with username and password
+	// registered in database
+	// returns true if exists or false otherwise
+	public boolean userValid(String username, String password);
+	
+	// method for checking if the user with username is logged in
+	// returns true if he is or false otherwise
+	public boolean isLogged(String username);
+	
+	// method for notifying the user with "username" the "status" of the "offer"
+	// has been changed.
+	public boolean notifyStatusChanged(String username, Offer offer, String status);
 }
